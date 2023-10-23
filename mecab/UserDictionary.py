592ea11057183a72a1710029ca06a2b3305b3a7f
hunhak <hn99709@naver.com>
2023-10-23 18:11:55 +0900

    no message

---
+++ mecab/UserDictionary.py
@@ -0,0 +1,14 @@
+import csv
+import subprocess
+import sys
+
+val = sys.argv[1]
+header = [val,'1786','3546','0','NNP','*','T',val,'*','*','*','*','*']
+with open(r"C:\mecab\mecab-ko-dic\user-custom.csv", 'a', newline='', encoding='utf-8-sig') as csv_file: 
+    writer = csv.writer(csv_file, delimiter=',', quotechar='"')
+    writer.writerow(header)
+
+p = subprocess.Popen(["powershell.exe", "C:\\mecab\\tools\\compile-win.ps1"], stdout=sys.stdout)
+p.communicate()
+
+print("Sucess")
